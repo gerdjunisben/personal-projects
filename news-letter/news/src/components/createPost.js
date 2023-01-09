5622c0915c557dc8a7579341af5127297c0a4908
Benjamin Gerdjunis <benjamingerdjunis@gmail.com>
2023-01-08 18:36:29 -0800

    Finished post uploading

--- news-letter/news/src/components/createPost.js
+++ news-letter/news/src/components/createPost.js
@@ -1,16 +1,64 @@
-import React from "react"
+import React, { useState } from "react";
+import {addDoc,collection} from "firebase/firestore"
+import {db,storage} from "../firebase-config";
+import { useNavigate } from "react-router-dom";
+import {ref,uploadBytesResumable,getDownloadURL} from "firebase/storage";
 
 function CreatePost(){
+    const [title,setTitle] = useState("");
+    const [file,setFile] = useState(null);
+    const [percent,setPercent] = useState(0);
+
+    const handleUpload = async (f)=> {
+        if (!f) {
+            alert("Please choose a file first!")
+        }
+        const storageRef = ref(storage,`/files/${f.name}`)
+        const uploadTask = uploadBytesResumable(storageRef, f);
+        uploadTask.on("state_changed",(snapshot) => {
+                const percent = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                setPercent(percent);
+            },
+            (err) => console.log(err),
+            () => {getDownloadURL(uploadTask.snapshot.ref).then(async (url) => {
+                await addDoc(postsCollectionRef,{
+                    title,
+                    url,
+                    date:{month:(new Date().getMonth()+1),year:new Date().getFullYear()}
+                });
+            });
+        }
+        );
+    }
+        
+ 
+
+    const postsCollectionRef=collection(db,"posts");
+    let navigate = useNavigate();
+    const createPost = async ()=>{
+        handleUpload(file);  
+        navigate("/");
+    }
+
     return( 
     <div>
         <h1>Create Post</h1>
         <div>
-            <label>Title</label>
-            <input type={"text"}/>
-            <label>pdf</label>
-            <input type={"file"}/>
+            <div>
+                <label>Title</label>
+                <input type={"text"} onChange={(e)=>{
+                    setTitle(e.target.value);
+                }}/>
+            </div>
+            <div>
+                <label>pdf</label>
+                <input type={"file"} onChange={(e)=>{
+                    setFile(e.target.files[0]);
+                }}/>
+            </div>
         </div>
-        <button>Submit Post</button>
+        <button onClick={createPost}>Submit Post</button>
+        <p>{percent}</p>
     </div>);
 
 }
--- news-letter/news/src/firebase-config.js
+++ news-letter/news/src/firebase-config.js
@@ -1,6 +1,7 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD4tKl4jeYPfkbEBn3TLe5t9E0JakCFn_o",
@@ -15,5 +16,6 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 export const provider = new GoogleAuthProvider();
 
