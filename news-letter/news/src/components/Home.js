163636ebf6d7af182afdbd6732a621498850e54e
Benjamin Gerdjunis <benjamingerdjunis@gmail.com>
2023-01-15 00:47:47 -0500

    finished all but the sign up

--- news-letter/news/src/components/Home.js
+++ news-letter/news/src/components/Home.js
@@ -1,7 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import {getDocs,collection} from "firebase/firestore"
+import {db} from "../firebase-config"
 
 function Home(){
-    return <div>hi there</div>
+    const [postLists,setPostList] = useState([]);
+
+    useEffect(()=>{
+        const getPosts = async ()=>{
+            const data = await getDocs(collection(db,"posts"));
+            setPostList(data.docs.map((doc)=>({...doc.data(),id:doc.id})));
+        }
+        getPosts();
+    });
+
+    return <div>
+        {postLists.map((post)=>{
+            return <div>
+                <a href= {post.url} rel="noreferrer" target="_blank">{post.title} {post.date.month}/{post.date.year}</a>
+            </div>
+
+        })}
+    </div>
 }
 
 export default Home;
